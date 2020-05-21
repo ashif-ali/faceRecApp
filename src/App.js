d63b8f38bae3a0fc49ebc6a338523611c652d776
Ashif <ashifali2327@outlook.com>
2020-05-21 16:33:35 +0530

    registration form added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 // import logo from './logo.svg';
 import './App.css';
 import 'tachyons';
@@ -31,7 +32,8 @@ class App extends Component {
 		input: '',
 		imageUrl: '',
 		box: {},
-		route: 'signin'
+		route: 'signin',
+		isSignedIn: false
 	};
 
 	calculateFaceLocation = (data) => {
@@ -64,34 +66,36 @@ class App extends Component {
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
 			.then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
-			// function(response) {
-			// 	console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-			// },
-			// function(error) {
-			// 	//error
-			// }
 			.catch((err) => console.log(err));
 	};
 
 	onRouteChange = (route) => {
+		if (route === 'signout') {
+			this.setState({ isSignedIn: false });
+		} else if (route === 'home') {
+			this.setState({ isSignedIn: true });
+		}
 		this.setState({ route: route });
 	};
 
 	render() {
+		const { isSignedIn, imageUrl, route, box } = this.state;
 		return (
 			<div className="App">
 				<Particles className="particles" params={particleOptions} />
 
-				<Navigation onRouteChange={this.onRouteChange} />
-				{this.state.route === 'signin' ? (
-					<SignIn onRouteChange={this.onRouteChange} />
-				) : (
+				<Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+				{route === 'home' ? (
 					<div>
 						<Logo />
 						<Rank />
 						<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-						<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+						<FaceRecognition box={box} imageUrl={imageUrl} />
 					</div>
+				) : route === 'signin' ? (
+					<SignIn onRouteChange={this.onRouteChange} />
+				) : (
+					<Register onRouteChange={this.onRouteChange} />
 				)}
 			</div>
 		);
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
 	return (
 		<div>
 			<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 				<main className="pa4 black-80">
 					<div className="measure">
 						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-							<legend className="f1 fw6 ph0 mh0">Sign In</legend>
+							<legend className="f1 fw6 ph0 mh0">Register</legend>
+							<div className="mt3">
+								<label className="db fw6 lh-copy f6" htmlFor="email-address">
+									Name
+								</label>
+								<input
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+									type="text"
+									name="name"
+									id="name"
+									required
+								/>
+							</div>
 							<div className="mt3">
 								<label className="db fw6 lh-copy f6" htmlFor="email-address">
 									Email
@@ -37,14 +49,9 @@ const SignIn = ({ onRouteChange }) => {
 							<input
 								className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
 								type="submit"
-								value="Sign in"
+								value="Register"
 								onClick={() => onRouteChange('home')}
 							/>
-						</div>
-						<div className="lh-copy mt3">
-							<p onClick={() => onRouteChange('register')} className="f6 pointer link dim black db">
-								Register
-							</p>
 						</div>
 					</div>
 				</main>
@@ -53,4 +60,4 @@ const SignIn = ({ onRouteChange }) => {
 	);
 };
 
-export default SignIn;
+export default Register;
