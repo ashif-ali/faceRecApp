a741ba497be722351fa44241c2eac831e09e617f
Ashif <ashifali2327@outlook.com>
2020-05-30 22:14:16 +0530

    Updates

---
+++ precache-manifest.c85cf345c7d24a31fe9b013efee35d77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61815590827e01b6ddf6c3287e1bfc8",
+    "url": "/faceRecApp/index.html"
+  },
+  {
+    "revision": "6c84372f6762bce4b9da",
+    "url": "/faceRecApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ae9c25c9ef9364ff65a7",
+    "url": "/faceRecApp/static/css/main.56b90ac6.chunk.css"
+  },
+  {
+    "revision": "6c84372f6762bce4b9da",
+    "url": "/faceRecApp/static/js/2.1f42dc08.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/faceRecApp/static/js/2.1f42dc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9c25c9ef9364ff65a7",
+    "url": "/faceRecApp/static/js/main.edda1926.chunk.js"
+  },
+  {
+    "revision": "44e429484d4804eb8262",
+    "url": "/faceRecApp/static/js/runtime-main.84393f5e.js"
+  }
+]);
